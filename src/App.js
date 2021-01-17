2aed380d998939207b9a6cb6f4ef1500a8694555
Gautam Bohara <Gautam_Bohara@BGMB124197-2.local>
2021-01-17 11:29:20 +0530

    initial Setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text>Hello World! </Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
